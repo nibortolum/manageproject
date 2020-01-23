c4b23165cfca2927074b751432645afaad5331d4
nibortolum <matthieu.mulot@datatailors.ch>
2020-01-23 22:05:28 +0100

    Initial commit

---
+++ R/make_project.R
@@ -0,0 +1,11 @@
+#' Create a now project directory
+#'
+#' @param path Absolute of relative project path. If the directory does not exist, it will be created.
+#'
+#' @return A beautifully organised project folder
+#' @export
+#'
+#' @examples
+make_project <- function(path){
+  
+}
